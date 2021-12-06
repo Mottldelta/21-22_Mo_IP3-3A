007fe2ec98d5ef082dcc455468a8e2ff9f96a8d4
Jan Mottl <mottl.jan96@gmail.com>
2021-12-06 08:59:19 +0100

    SQL zakladní dotazy dokončení

--- OpakovaniSQL.sql
+++ OpakovaniSQL.sql
@@ -57,3 +57,42 @@ SELECT *
 FROM employees
 WHERE name LIKE 'B%y'
 
+-- JOIN
+SELECT zamestatnanci.Adresa,pracoviste.Nazev
+FROM zamestatnanci
+         INNER JOIN pracoviste ON zamestatnanci.IDpracoviste=pracoviste.ID;
+
+-- UPDATE
+UPDATE `employees` S
+SET `country` = 'Brazil'
+WHERE `employees`.`id` = 14;
+
+-- DELETE
+DELETE FROM employees
+WHERE id BETWEEN 5 AND 10;
+
+-- LIMIT
+SELECT name
+FROM employees
+WHERE name LIKE 'c%'
+    LIMIT 3;
+
+-- MIN a MAX
+SELECT MAX(id)
+FROM employees;
+
+-- COUNT
+SELECT COUNT(*)
+FROM employees
+WHERE country IN ('Brazil', 'China', 'India', 'Colombia', 'Vietnam', 'Nigeria', 'Chile', 'Pakistan', 'Netherlands');
+
+-- ORDER BY
+SELECT *
+FROM employees
+ORDER BY name, address
+
+-- GROUP BY
+SELECT COUNT(id), address
+FROM employees
+GROUP BY address
+
