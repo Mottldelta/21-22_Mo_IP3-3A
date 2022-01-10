ba7333f04c7d4d675c2472b39483dee2ace6d684
Jan Mottl <mottl.jan96@gmail.com>
2022-01-10 09:11:54 +0100

    MySQL – Základ Banka - get (detail)

--- MySQL/banks/admin.php
+++ MySQL/banks/admin.php
@@ -5,11 +5,12 @@ $password = "";
 $dbname = "banks";
 
 $get = $_GET['id'];
+$value = $_GET['suma'];
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "UPDATE customers SET currency = currency - 20 WHERE id = '$get'";
+    $sql = "UPDATE customers SET currency = currency - $value WHERE id = '$get'";
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "Poplatek byl stržen" . "<br>";
--- MySQL/banks/detail.php
+++ MySQL/banks/detail.php
@@ -25,10 +25,12 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 
-$adress1 = "admin.php?id=$get&suma=";
+$adress1 = "admin.php?id=$get&suma=20";
 echo "<a href=". $adress1 .">Strhnout poplatek za kreditní kartu</a>" . "<br>";
-$adress2 = "admin.php?id=$get&suma=";
+$adress2 = "admin.php?id=$get&suma=100";
 echo "<a href=". $adress2 .">Strhnout poplatek za vedení účtu</a>" . "<br>";
+$adress3 = "admin.php?id=$get&suma=100";
+echo "<a href=". $adress3 .">Vynulovat účet</a>" . "<br>";
 $adress = "seznam.php";
 echo "<a href=". $adress .">Zpět na seznam zákazníků</a>" . "<br>";
 ?>
