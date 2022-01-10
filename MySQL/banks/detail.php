b787f95aef565ff628ecc54c766e8dad3a298e50
Jan Mottl <mottl.jan96@gmail.com>
2022-01-10 08:41:56 +0100

    MySQL – Základ Banka - get (Seznam)

--- MySQL/banks/detail.php
+++ MySQL/banks/detail.php
@@ -9,21 +9,23 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT id, name, currency FROM customers";
+$get = $_GET['id'];
+$sql = "SELECT id, name, email, country, currency FROM customers WHERE id = '$get'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        $id = $row["id"];
-        $adresa = "detail.php?id=$id";
-        $link = $row["name"];
-        echo "<a href=" . $adresa .">$link</a>" . "<br>";
+        echo "<b>" . $row["name"] . "</b>" . "<br>";
+        echo $row["email"] . "<br>";
+        echo $row["country"] . "<br>";
+        echo "Zůstatek na účtě činí: " . $row["currency"] . "<br>";
     }
 } else {
     echo "0 results";
 }
 $conn->close();
 
-$adress = "index.php";
-echo "<a href=". $adress .">Zpět na hlavní stránku</a>" . "<br>";
+$adress = "seznam.php";
+echo "<a href=". $adress .">Zpět na seznam zákazníků</a>" . "<br>";
 ?>
+
--- MySQL/banks/index.php
+++ MySQL/banks/index.php
@@ -6,5 +6,7 @@ $adress2 = "seznam.php";
 echo "<a href=". $adress2 .">Seznam zákazníků</a>" . "<br>";
 $adress3 = "seznamspod.php?country=Spain";
 echo "<a href=". $adress3 .">Seznam zákazníků ze Španělska</a>" . "<br>";
+$adress4 = "seznamspod.php?country=Italy";
+echo "<a href=". $adress4 .">Seznam zákazníků z Itálie</a>" . "<br>";
 
 ?>
