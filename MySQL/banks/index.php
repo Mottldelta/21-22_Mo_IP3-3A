6fd396e023c7c40b4735bc09eab5ac15586d07bf
Jan Mottl <mottl.jan96@gmail.com>
2022-01-10 08:22:13 +0100

    MySQL – Základ Banka - get

--- MySQL/banks/index.php
+++ MySQL/banks/index.php
@@ -4,5 +4,7 @@ $adress = "vloznahod.php";
 echo "<a href=". $adress .">Vlož náhodného zákazníka</a>" . "<br>";
 $adress2 = "seznam.php";
 echo "<a href=". $adress2 .">Seznam zákazníků</a>" . "<br>";
+$adress3 = "seznamspod.php?country=Spain";
+echo "<a href=". $adress3 .">Seznam zákazníků ze Španělska</a>" . "<br>";
 
 ?>
