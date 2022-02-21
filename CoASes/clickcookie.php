13363325a6976b35d061fcadaee888dc226daa2c
Jan Mottl <mottl.jan96@gmail.com>
2022-02-21 09:29:14 +0100

    Form PHP cookies (click final)

--- CoASes/clickcookie.php
+++ CoASes/clickcookie.php
@@ -1,6 +1,8 @@
 <?php
+//Pokud ještě uživatel nehrál, tak se musí nastavit nová hra skrz set-up cookie s názvem "klik", v druhém případě se vypíše cookie a její hodnota.
 if(!isset($_COOKIE['klik'])) {
     setcookie('klik',0,time()+2000,"/");
+    echo "Počet kliknutí je " . $_COOKIE['klik'] . "<br>";
 } else {
     echo "Počet kliknutí je " . $_COOKIE['klik'] . "<br>";
 }
@@ -13,8 +15,9 @@ if(!isset($_COOKIE['klik'])) {
 </html>
 
 <?php
-
+//funkce se volá v případě kliknutí na button v rámci form v html. Následně zvyšuje číslo a přepisuje hodnotu cookie "klik"
 function click() {
-
+ $numclick = $_COOKIE['klik'] + 1;
+ setcookie('klik',$numclick,time()+2000,"/");
 }
 ?>
