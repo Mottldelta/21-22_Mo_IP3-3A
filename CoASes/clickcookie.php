cbd4812cd09cee66996c02c3ea0a9ac7c454a48b
Jan Mottl <mottl.jan96@gmail.com>
2022-02-21 09:08:49 +0100

    Form PHP cookies (click v.2)

--- CoASes/clickcookie.php
+++ CoASes/clickcookie.php
@@ -1,11 +1,17 @@
 <?php
-
+if(!isset($_COOKIE['klik'])) {
+    setcookie('klik',0,time()+2000,"/");
+} else {
+    echo "Počet kliknutí je " . $_COOKIE['klik'] . "<br>";
+}
 ?>
+
 <html>
 <form>
     <button onclick="<?php click() ?>">Click!</button>
 </form>
 </html>
+
 <?php
 
 function click() {
