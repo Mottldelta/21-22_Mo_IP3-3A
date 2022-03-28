8abd39d6dfe7e5603848f23a24d66a16170b8ab6
Jan Mottl <mottl.jan96@gmail.com>
2022-03-28 08:28:55 +0200

    Form PHP práce se soubory v.2

--- DalFunk/soubor.php
+++ DalFunk/soubor.php
@@ -12,10 +12,35 @@ fclose($text);
 
 //otevření souboru
 $text1 = fopen('text.txt','r');
-//čtení po řádcích
-echo fgets($text1). "<br>";
-echo fgets($text1). "<br>";
-echo fgets($text1). "<br>";
+//čtení po řádcích do konce souboru
+while(!feof($text1)) {
+    echo fgets($text1) . "<br>";
+}
 //zavření souboru
 fclose($text1);
+
+//otevření souboru
+$text2 = fopen('text.txt','r');
+//čtení po znacích do konce souboru
+while(!feof($text2)) {
+    echo fgetc($text2) . "<br>";
+}
+//zavření souboru
+fclose($text2);
+
+//otevření souboru
+$text3 = fopen('text.txt','w');
+//zapisování do souboru
+fwrite($text3, "Ahoj \nAHoj \nAhoj");
+//zavření souboru
+fclose($text3);
+
+//otevření souboru
+$text3 = fopen('text.txt','a');
+//dopisování do souboru
+fwrite($text3, "Ahoj \nAHoj \nAhoj");
+//zavření souboru
+fclose($text3);
+
+
 ?>
